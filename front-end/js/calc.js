2f8aabd4e7f582ac86cc25b9d70306ed2f87d460
Puneet Vashisht <puneetvashisht@outlook.com>
2022-03-02 17:08:35 +0530

    jquery & css

--- front-end/js/calc.js
+++ front-end/js/calc.js
@@ -1,7 +1,32 @@
 function add(){
+  
     var oper1= document.getElementById("oper1").value
     var oper2= document.getElementById("oper2").value
     var result= parseInt(oper1)+parseInt(oper2)
+    console.log(result);
+
+    document.getElementById("result").innerText="Result is "+result
+}
+function sub(){
+    var oper1= document.getElementById("oper1").value
+    var oper2= document.getElementById("oper2").value
+    var result= oper1-oper2
+    console.log(result);
+
+    document.getElementById("result").innerText="Result is "+result
+}
+function mul(){
+    var oper1= document.getElementById("oper1").value
+    var oper2= document.getElementById("oper2").value
+    var result= oper1*oper2
+    console.log(result);
+
+    document.getElementById("result").innerText="Result is "+result
+}
+function divide(){
+    var oper1= document.getElementById("oper1").value
+    var oper2= document.getElementById("oper2").value
+    var result= oper1/oper2
     console.log(result);
 
     document.getElementById("result").innerText="Result is "+result
